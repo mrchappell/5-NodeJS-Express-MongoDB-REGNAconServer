cbc3c10e02c9f1e8aca9dc8598acf0fc587509b2
mrchappell <michael.r.chappell@gmail.com>
2020-02-28 23:24:48 -0600

    added Facebook

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const config = require('./config');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const celebRouter = require('./routes/celebRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
+const mongoose = require('mongoose');
+
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
+);
 
 const app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+}); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,29 +46,35 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/campsites', celebRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  app.use('/celebs', celebRouter);
+  app.use('/promotions', promotionRouter);
+  app.use('/partners', partnerRouter);
+  app.use('/imageUpload', uploadRouter);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-module.exports = app;
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  module.exports = app;
--- routes/celebRouter.js
+++ routes/celebRouter.js
@@ -1,29 +1,237 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Campsite = require('../models/celeb');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const celebRouter = express.Router();
 
 celebRouter.use(bodyParser.json());
 
 celebRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end('Will send all the celebrities to you');
-})
-.post((req, res) => {
-    res.end(`Will add the celebrities: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /celebs');
-})
-.delete((req, res) => {
-    res.statusCode = 403;
-    res.end('Deleting all celebs not supported');
-});
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req, res, next) => {
+        Celeb.find()
+            .populate('comments.author')
+            .then(celebs => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(celebs);
+            })
+            .catch(err => next(err));
+    })
+    .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.create(req.body)
+            .then(celeb => {
+                console.log('Celeb Created ', celeb);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(celeb);
+            })
+            .catch(err => next(err));
+    })
+    .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /celebs');
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.deleteMany()
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
+    });
 
-module.exports = celebRouter;
+    campsiteRouter.route('/:celebId')
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+    .get(cors.cors, (req, res, next) => {
+        Celeb.findById(req.params.celebId)
+            .populate('comments.author')
+            .then(celeb => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(celeb);
+            })
+            .catch(err => next(err));
+    })
+    .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /celebs/${req.params.celebId}`);
+    })
+    .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.findByIdAndUpdate(req.params.celebId, {
+            $set: req.body
+        }, { new: true })
+            .then(celeb => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(celeb);
+            })
+            .catch(err => next(err));
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.findByIdAndDelete(req.params.celebId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
+    });
+
+    celebRouter.route('/:celebId/comments')
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+    .get(cors.cors, (req, res, next) => {
+        Celeb.findById(req.params.celebId)
+            .populate('comments.author')
+            .then(celeb => {
+                if (celeb) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(celeb.comments);
+                } else {
+                    err = new Error(`Celeb ${req.params.campsiteId} not found`);
+                    err.status = 404;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    })
+    .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.findById(req.params.celebId)
+            .then(celeb => {
+                if (celeb) {
+                    req.body.author = req.user._id;
+                    celeb.comments.push(req.body);
+                    celeb.save()
+                        .then(celeb => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(campsite);
+                        })
+                        .catch(err => next(err));
+                } else {
+                    err = new Error(`Celeb ${req.params.celebId} not found`);
+                    err.status = 404;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    })
+    .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.statusCode = 403;
+        res.end(`PUT operation not supported on /celebs/${req.params.celebId}/comments`);
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.findById(req.params.celebId)
+            .then(celeb => {
+                if (celeb) {
+                    for (let i = (celeb.comments.length - 1); i >= 0; i--) {
+                        celeb.comments.id(celeb.comments[i]._id).remove();
+                    }
+                    celeb.save()
+                        .then(celeb => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(celeb);
+                        })
+                        .catch(err => next(err));
+                } else {
+                    err = new Error(`Celeb ${req.params.celebId} not found`);
+                    err.status = 404;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    });
+
+    celebRouter.route('/:celebId/comments/:commentId')
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+    .get(cors.cors, (req, res, next) => {
+        Celeb.findById(req.params.celebId)
+            .populate('comment.author')
+            .then(celeb => {
+                if (celeb && celeb.comments.id(req.params.celebId)) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(celeb.comments.id(req.params.commentId));
+                } else if (!celeb) {
+                    err = new Error(`Celeb ${req.params.celebId} not found`);
+                    err.status = 404;
+                    return next(err);
+                } else {
+                    err = new Error(`Comment ${req.params.commentId} not found`);
+                    err.status = 404;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    })
+    .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`);
+    })
+    .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.findById(req.params.celebId)
+            .then(celeb => {
+                if (celeb && celeb.comments.id(req.params.commentId)) {
+                    if ((celeb.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
+                        celeb.comments.id(req.params.commentId).remove();
+                        Celeb.save()
+                            .then(celeb => {
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(celeb);
+                            })
+                            .catch(err => next(err));
+                    } else if (!celeb) {
+                        err = new Error(`Celeb ${req.params.celebId} not found`);
+                        err.status = 404;
+                        return next(err);
+                    } else {
+                        err = new Error(`Comment ${req.params.commentId} not found`);
+                        err.status = 404;
+                        return next(err);
+                    }
+                } else {
+                    err = new Error('This is not your comment to update.');
+                    err.status = 403;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Celeb.findById(req.params.celebId)
+            .then(celeb => {
+                if (celeb && celeb.comments.id(req.params.commentId)) {
+                    if ((celeb.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
+                        celeb.comments.id(req.params.commentId).remove();
+                        celeb.save()
+                            .then(celeb => {
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(celeb);
+                            })
+                            .catch(err => next(err));
+                    } else if (!celeb) {
+                        err = new Error(`Celeb ${req.params.celebId} not found`);
+                        err.status = 404;
+                        return next(err);
+                    } else {
+                        err = new Error(`Comment ${req.params.commentId} not found`);
+                        err.status = 404;
+                        return next(err);
+                    }
+                } else {
+                    err = new Error('This is not your comment to update.');
+                    err.status = 403;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    });
+
+module.exports = campsiteRouter;
